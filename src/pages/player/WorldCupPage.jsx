6949691e560dbeab0656597d3f7b4cc97e3629fb
CPrice2024 <redhamikael@gmail.com>
2026-07-18 10:35:45 +0300

    featured event 8

--- src/pages/player/WorldCupPage.jsx
+++ src/pages/player/WorldCupPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api";
-import { X } from "lucide-react";
+import {  } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 import "../../styles/worldCupPage.css";
